1c8e747d7df1b9368aacc938a68d53484fe0942a
lawal lanre <larrystonegroup@gmail.com>
2017-12-14 00:56:03 +0100

    feat(chat): implement chat feature in game - setup dependencies (firebase, angular-fire, et al) - create chat User Interface - setup and implement chat controller - implement flash notification for new message - implement audio notification for new message - implement notification badge (with unread count) for new message(s) [Delivers #152796608]

---
+++ public/js/controllers/chat.js
@@ -0,0 +1,74 @@
+function scrollMessages() {
+  var chats = $('.chat_messages');
+  setTimeout(() => {
+    scrollToTop(chats);
+  }, 200);
+}
+
+function scrollToTop(chats) {
+  chats.animate({
+    scrollTop: chats[0].scrollHeight
+  }, 1000);
+}
+
+angular.module('mean.system')
+  .controller('chatController', ['$scope', 'game', '$firebaseArray', function ($scope, game, $firebaseArray) {
+    const ref = new Firebase(`https://elendil-cfh.firebaseio.com/${game.gameID}`);
+    $scope.isChatOpen = false;
+    let oldLength = 0, newLength = 0;
+    $scope.unreadCount = null;
+
+    const firstTime = true;
+
+    $scope.chats = $firebaseArray(ref);
+    if (firstTime) {
+      ref.remove();
+    }
+
+    $scope.chats.$watch(function (e) {
+      if (!$scope.isChatOpen) {
+        playTone();
+        newLength = $scope.chats.length;
+        $scope.unreadCount = newLength - oldLength;
+        var { author, message } = $scope.chats[newLength - 1];
+        toastr.clear();
+        toastr.info(message, author);
+      }
+      else {
+        oldLength = $scope.chats.length;
+        $scope.unreadCount = null;
+      }
+    });
+
+    $scope.addChat = function () {
+      if ($scope.messageInput) {
+        $scope.isChatOpen = true;
+        $scope.newMessage = {
+          author: game.players[game.playerIndex].username,
+          avatar: game.players[game.playerIndex].avatar,
+          message: $scope.messageInput,
+          time: new Date().toISOString(),
+        };
+        $scope.chats.$add($scope.newMessage);
+        $scope.messageInput = '';
+        scrollMessages();
+      }
+    };
+
+    $scope.showTime = function (time) {
+      return moment(time).fromNow();
+    };
+
+    $scope.openChat = function () {
+      toastr.clear();
+      $scope.isChatOpen = !$scope.isChatOpen;
+      oldLength = newLength;
+      $scope.unreadCount = null;
+    };
+  }]);
+
+const playTone = function () {
+  const audio = new Audio('../../sounds/newMessage.mp3');
+  audio.volume = 0.2;
+  audio.play();
+};
